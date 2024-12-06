34752da98b373f28700940efdbf95df8e9618e25
leesangjun <hubls@naver.com>
2024-12-06 16:32:24 +0900

    first commit

---
+++ src/main/java/com/example/bucket4jclientip/Bucket4jClientipApplication.java
@@ -0,0 +1,13 @@
+package com.example.bucket4jclientip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bucket4jClientipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bucket4jClientipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bucket4jclientip/Bucket4jClientipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bucket4jclientip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bucket4jClientipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
